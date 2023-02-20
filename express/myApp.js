cdc8d8e913f39ed5fcafe1e1bbc4316b7277e371
Renzo Posobon <renzoposobon95@gmail.com>
2023-02-20 09:39:34 -0300

    npm & express

---
+++ express/myApp.js
@@ -0,0 +1,59 @@
+let express = require('express');
+let app = express();
+var bGround = require('fcc-express-bground');
+var myApp = require('./myApp');
+
+
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+    console.log("Hello World");
+  });
+}
+
+var port = process.env.PORT || 3000;
+bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
+  bGround.log('Node is listening on port '+ port + '...')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
