2b5c6436127ddb1401612c1c12c4228f326edb4a
Renzo Posobon <renzoposobon95@gmail.com>
2023-02-24 15:54:54 -0300

    finalizacion micro3

--- microservicio_3/index.js
+++ microservicio_3/index.js
@@ -1,95 +1,65 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
 const app = express();
+
+// Basic Configuration
 const port = process.env.PORT || 3000;
-const urlRegex = /^https?:\/\/(?:www\.)?[\w\.-]+\.[a-z]{2,}(?:\/[\w\.\/%&=]*)?$/;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true}))
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-});
-
-const urlSchema = new mongoose.Schema({
-  original_url: String,
-  short_url: Number
-});
-
-const Url = mongoose.model('Url', urlSchema);
-let currentShortUrl = 1;
-
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/hello', (req, res) => {
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const originalUrls = []
+const shortUrls = []
+
 app.post('/api/shorturl', (req, res) => {
-  const url = req.body.url;
-  if (urlRegex.test(url)) {
-    Url.findOne({ original_url: url }, (err, foundUrl) => {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUrl) {
-          res.send({
-            original_url: foundUrl.original_url,
-            short_url: foundUrl.short_url
-          });
-        } else {
-          const newUrl = new Url({
-            original_url: url,
-            short_url: currentShortUrl
-          });
-          newUrl.save((err, savedUrl) => {
-            if (err) {
-              console.log(err);
-            } else {
-              currentShortUrl++;
-              res.send({
-                original_url: savedUrl.original_url,
-                short_url: savedUrl.short_url
-              });
-            }
-          });
-        }
-      }
-    });
-  } else {
-    res.json({ error: 'invalid url' });
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+
+  if (foundIndex < 0) {
+    originalUrls.push(url)
+    shortUrls.push(shortUrls.length)
+    
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1
+    })
   }
-});
 
-app.get('/api/shorturl/:shortUrl', (req, res) => {
-  const shortUrl = Number(req.params.shortUrl);
-  Url.findOne({ short_url: shortUrl }, (err, foundUrl) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUrl) {
-        res.redirect(foundUrl.original_url);
-      } else {
-        res.json({ error: 'invalid short url' });
-      }
-    }
-  });
-});
+  if(!url.includes("https://" && !url.includes("http://"))) {
+    return res.json({ 'error': 'invalid url' })
+  }
 
-app.listen(port, () => {
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  })
+})
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shorturl = parseInt(req.params.shorturl)
+  const foundIndex = shortUrls.indexOf(shorturl)
+
+  if (foundIndex < 0) {
+    return res.json({
+      "error": "No short URL found for the given input"
+    })
+  }
+
+  res.redirect(originalUrls[foundIndex])
+})
+
+app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
